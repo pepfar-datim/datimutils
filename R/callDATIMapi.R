e1bdb007b9cea308280975f366657eb2e5c89951
sam-bao <sgarman@baosystems.com>
2020-04-16 13:32:09 +0200

    JUST style changes

--- R/callDATIMapi.R
+++ R/callDATIMapi.R
@@ -1,49 +1,55 @@
 #' @export
 #' @title Execute and return a DATIM API query.
-#' 
-#' @description
-#' Gets and flattens DATIM API query as dataframe.
-#' 
+#' @description Gets and flattens DATIM API query as dataframe.
 #' @param path Should begin with api/ and contain the query
 #' @param base_url the url on which is added the path
-#' @param retry number of times to try in case of failure, default will not try again
+#' @param retry number of times to try in case of failure,
+#' default will not try again
 #' @param timeout how long should a reponse be waited for
 #' @param api_version defaults to current but can pass in version number
-#' 
 #' @return Result of DATIM API query returned as named list.
-#' 
-api_get <- function(path, base_url = getOption("baseurl"), retry = 1, timeout = 60,
-                    api_version = NULL ) {
-  if(!(grepl("api",substr(path,1,4)))){
-      path <- paste0("api/",path)
-    }
-  if(grepl("api",substr(path,1,4))){
-  path <- sub( '(?<=.{4})', ifelse(is.null(api_version), "", paste0(api_version, "/")), 
-               path, perl=TRUE )}
+#'
+api_get <- function(path, base_url = getOption("baseurl"),
+                    retry = 1, timeout = 60,
+                    api_version = NULL) {
+  if (!(grepl("api", substr(path, 1, 4)))) {
+    path <- paste0("api/", path)
+  }
+  if (grepl("api", substr(path, 1, 4))) {
+    path <- sub("(?<=.{4})", ifelse(is.null(api_version), "",
+      paste0(api_version, "/")
+    ),
+    path,
+    perl = TRUE
+    )
+  }
   url <- paste0(url = base_url, path = path)
-  if(grepl("\\/\\?",url))
-  {
-    url <-sub("(.*?)(\\//?)", "\\1.json?paging=false\\2", url)
-  }else if(grepl("\\?",url))
-  {
-    url <-sub("(.*?)(\\?)", "\\1.json?paging=false\\2", url)
-  }else{url <- paste0(url,".json?paging=false")}
-  url <- gsub("///", "/",url)
-  url <- gsub("//", "/",url)
-  i = 1; response_code = 5
-  while(i <= retry & response_code != 200 ){
+  if (grepl("\\/\\?", url)) {
+    url <- sub("(.*?)(\\//?)", "\\1.json?paging=false\\2", url)
+  } else if (grepl("\\?", url)) {
+    url <- sub("(.*?)(\\?)", "\\1.json?paging=false\\2", url)
+  } else {
+    url <- paste0(url, ".json?paging=false")
+  }
+  url <- gsub("///", "/", url)
+  url <- gsub("//", "/", url)
+  i <- 1
+  response_code <- 5
+  while (i <= retry & response_code != 200) {
     resp <- httr::GET(url, httr::timeout(timeout))
     response_code <- httr::status_code(resp)
-    i = i+1
+    i <- i + 1
   }
   if (httr::http_type(resp) != "application/json") {
-    stop(paste0("API did not return json, are you logged into DATIM? 
-         If not please use loginToDatim function \n url is "), url ,"\n", "cookie is", 
-         httr::cookies(resp))}
+    stop(
+      paste0("API did not return json, are you logged into DATIM?
+         If not please use loginToDatim function \n url is "), url,
+      "\n", "cookie is", httr::cookies(resp)
+    )
+  }
   if (httr::status_code(resp) != 200) {
-    stop(paste0("api query failed for url ", url))}
-  resp <- jsonlite::fromJSON(httr::content(resp,as = "text"), flatten = T)
+    stop(paste0("api query failed for url ", url))
+  }
+  resp <- jsonlite::fromJSON(httr::content(resp, as = "text"), flatten = T)
   return(resp)
 }
-
-
--- R/loginToDATIM.R
+++ R/loginToDATIM.R
@@ -3,47 +3,51 @@
 #'
 #' @description Loads a JSON configuration file to access a DHIS2 instance
 #' @param config_path Path to the DHIS2 credentials file
-#' @return A parsed list of the configuration file. 
+#' @return A parsed list of the configuration file.
 #'
 loadConfigFile <- function(config_path = NA) {
-  #Load from a file
+  # Load from a file
   if (!is.na(config_path)) {
     if (file.access(config_path, mode = 4) == -1) {
-      stop(paste("Cannot read configuration located at",config_path))
+      stop(paste("Cannot read configuration located at", config_path))
     }
-    
+
     dhis_config <- jsonlite::fromJSON(config_path)
     options("baseurl" = dhis_config$dhis$baseurl)
     options("config" = config_path)
     return(dhis_config)
   } else {
-    stop("You must specify a credentials file!") }
+    stop("You must specify a credentials file!")
+  }
 }
 
 #' @export
-#' @title makeKeyring(ring ="DatimLogin", service = getOption("baseurl"), username)
+#' @title makeKeyring(ring ="DatimLogin",
+#' service = getOption("baseurl"), username)
 #'
 #' @description makes a new keyRing
-#' @param username username 
+#' @param username username
 #' @param ring keyring name
 #' @param service baseurl
 #' @return none
 #' @details ENTER FIRST KEYCHAIN PASSWORD THEN SECRET
 #'
-makeKeyring <- function (username, 
-                         ring ="DatimLogin", 
-                         service = getOption("baseurl")) 
-{
-
-result <- try(keyring::key_list(keyring = ring),silent = T)
-  if("try-error" %in% class(result)){
-    error_type <- attr(result,"condition")
-    if(grepl("The specified keychain could not be found",error_type$message)){
-        print("enter KEYCHAIN password, then enter SECRET")
-        keyring::keyring_create(ring)
-        keyring::key_set(service, username, keyring = ring)
-        keyring::keyring_lock(ring)
-      }}else{print("keychain already exists")}  
+makeKeyring <- function(username,
+                        ring = "DatimLogin",
+                        service = getOption("baseurl")) {
+  result <- try(keyring::key_list(keyring = ring), silent = T)
+  if ("try-error" %in% class(result)) {
+    error_type <- attr(result, "condition")
+    if (grepl("The specified keychain could not be found",
+              error_type$message)) {
+      print("enter KEYCHAIN password, then enter SECRET")
+      keyring::keyring_create(ring)
+      keyring::key_set(service, username, keyring = ring)
+      keyring::keyring_lock(ring)
+    }
+  } else {
+    print("keychain already exists")
+  }
 }
 
 #' @export
@@ -53,39 +57,47 @@ result <- try(keyring::key_list(keyring = ring),silent = T)
 #' @param ring keyring name
 #' @return a list containing entries called password, baseurl, and username
 #'
-getCredentialsFromKeyring <- function(ring) 
-{
+getCredentialsFromKeyring <- function(ring) {
   try <- as.list(keyring::key_list(keyring = ring))
-  credentials = c( "password" = keyring::key_get(try[["service"]]), try)
+  credentials <- c("password" = keyring::key_get(try[["service"]]), try)
   names(credentials) <- c("password", "baseurl", "username")
   keyring::keyring_lock(ring)
   return(credentials)
 }
 
 #' @export
-#' @title loginToDATIMfunction(ring =NULL, config_path=NULL, config_path_level = "dhis" )
-#' @description logins into a datim or dhis2 api using either a keyring or a config file
-#' @param config_path path to a dhis config file, ring will bypass this option if not null
-#' @param config_path_level if there a multiple json entries in the config file, it will default to dhis
+#' @title loginToDATIMfunction(ring =NULL, config_path=NULL,
+#' config_path_level = "dhis" )
+#' @description logins into a datim or dhis2 api using either a keyring or a
+#' config file
+#' @param config_path path to a dhis config file, ring will bypass this
+#' option if not null
+#' @param config_path_level if there a multiple json entries in the config
+#' file, it will default to dhis
 #' @param ring the name of the keyring to be used
 #'
-loginToDATIM<-function(ring =NULL, config_path=NULL, config_path_level = "dhis" ) {
-  
-  if(!is.null(config_path) & is.null(ring) ){
-    credentials = loadConfigFile(config_path = config_path)
-    credentials = credentials[[config_path_level]]
-  }else{credentials = getCredentialsFromKeyring(ring = ring)}
+loginToDATIM <- function(ring = NULL,
+                         config_path = NULL,
+                         config_path_level = "dhis") {
+  if (!is.null(config_path) & is.null(ring)) {
+    credentials <- loadConfigFile(config_path = config_path)
+    credentials <- credentials[[config_path_level]]
+  } else {
+    credentials <- getCredentialsFromKeyring(ring = ring)
+  }
 
-  url <- utils::URLencode(URL = paste0(credentials[["baseurl"]], "api","/me"))
-  #Logging in here will give us a cookie to reuse
-  r <- httr::GET(url ,
-                 httr::authenticate(credentials[["username"]], credentials[["password"]]),
-                 httr::timeout(60))
-  if(r$status != 200L){
+  url <- utils::URLencode(URL = paste0(credentials[["baseurl"]], "api", "/me"))
+  # Logging in here will give us a cookie to reuse
+  r <- httr::GET(
+    url,
+    httr::authenticate(credentials[["username"]], credentials[["password"]]),
+    httr::timeout(60)
+  )
+  if (r$status != 200L) {
     stop("Could not authenticate you with the server!")
   } else {
-    me <- jsonlite::fromJSON(httr::content(r,as = "text"))
-    print(paste0( me$name, " is logged in"))
+    me <- jsonlite::fromJSON(httr::content(r, as = "text"))
+    print(paste0(me$name, " is logged in"))
     options("organisationUnit" = me$organisationUnits$id)
   }
 }
