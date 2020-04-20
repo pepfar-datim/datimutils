c4512548eb5fe72adfe9771dd829c21998c0de48
maxwellchandler <mchandler@baosystems.com>
2020-04-20 17:12:13 -0600

    Add getOrgunit functions

--- R/getMetadata.R
+++ R/getMetadata.R
@@ -1,4 +1,75 @@
 #' @export
+#' @title processFilters(filters)
+#' @description takes a filter argument and turns it into an api compatible string
+#' @param filters wildcard argument that can come in as any format or datatype
+#' @return the processed metadata filter string compatible with DATIM api
+#'
+
+processFilters <- function(filters){
+  #takes filter argument and turns it into a single character string
+  ex <- stringr::str_flatten(unlist(sapply(filters, as.character)))
+  #removes extraneous info
+  look <- sub("\\?filter=", "", ex)
+  look <- sub("\\&filter=", "", ex)
+  #extracts end_point and what is not end_point
+  if (!(grepl("^id|name", look))) {
+    end_point <- stringr::str_extract(look, ".+?(?=id|name)")
+    look <- sub("(.*)(\\id|name)", "\\2", look)
+    end_point <- gsub("/", "", end_point)
+  }
+  #extracts the original filter 
+  filter_option_orig <-
+    stringr::str_extract(substr(look, 1, 10),
+                         "!ilike|!like|ilike|!in|!eq|in|eq|like")
+  #extracts either id or name from filter
+  filter_item <- stringr::str_extract(substr(look, 1, 10), "id|name")
+  #this block replaces the filter with one more adequate (eq=in, like=ilike, etc.)
+  if (grepl("eq", filter_option_orig)) {
+    filter_option <- sub("eq", "in", filter_option_orig)
+  } else if ("like" == filter_option_orig) {
+    filter_option <- sub("like", "ilike", filter_option_orig)
+  } else {
+    filter_option <- filter_option_orig
+  }
+  #creates a basic filter path
+  ex <- paste0(gsub(filter_option_orig, filter_option, substr(look, 1, 5)),
+               substr(look, 6, nchar(look)))
+  #removes :
+  ex <- gsub(":", "", ex)
+  #takes first part of filter, i.e. idin
+  one <- sub(paste0("(", filter_item, filter_option, ")", "(.*)"),
+             "\\1", ex)
+  one.one <- sub(paste0("(", filter_item, filter_option, ")", "(.*)"),
+                 "\\2", ex)
+  #takes second part of filter, i.e. ["abc"] and adds commas if filter = in
+  if(grepl("name",one)){
+    two <- gsub("(\\w)([A-Z])", "\\1,\\2",
+                one.one, perl = TRUE)
+  }else{
+    two <- ifelse(grepl(",", one.one), one.one,
+                  gsub("(.{11})", "\\1,",
+                       one.one, perl = TRUE))}
+  ex <- paste0(end_point, one, two)
+  if (substr(ex, nchar(ex), nchar(ex)) == ",") {
+    ex <- substr(ex, 1, nchar(ex) - 1)
+  }
+  #adds &filter= where needed, and : where needed
+  ex <- sub(paste0("(.*?)(\\&filter=|", filter_item, "|?filter=", ")"),
+            paste0("\\1&filter=", filter_item), ex)
+  ex <- sub(paste0("(.*?)", "(", filter_item, ")"),
+            paste0("\\1", filter_item, ":"), ex)
+  ex <- sub(paste0("(.*?)", "(", filter_option, ")"),
+            paste0("\\1", filter_option, ":"), ex)
+  #special processing for "in" filter
+  if (grepl("in", filter_option) & !(grepl("\\[", ex))) {
+    ex <- paste0(sub("(.*?)(in:)", "\\1in:[", ex), "]")
+  }
+  #removes whitespace
+  ex <- gsub(" ", "", ex)
+  return(ex)
+}
+
+#' @export
 #' @title getMetadata(base_url, end_point, filters, fields, pluck)
 #'
 #' @description General utility to get metadata details from DATIM
@@ -9,6 +80,8 @@
 #' components are present
 #' @param fields - the fields, which can come in any formt as long as all
 #' components are present
+#' @param filters2 - the other filters, which can come in any format as long as all
+#' components are present
 #' @param pluck - whether to add pluck option as documented by dhis2 api
 #' developer guide
 #' @param retry number of times to retry
@@ -16,7 +89,7 @@
 #'
 
 getMetadata <- function(end_point, base_url = getOption("baseurl"),
-                        filters = NULL, fields = NULL,
+                        filters = NULL, fields = NULL, filters2 = NULL,
                         pluck = F, retry = 1) {
   #if no filters or fields are specified, just use endpoint as path
   if (!(is.null(filters)) | !(is.null(fields))) {
@@ -24,66 +97,7 @@ getMetadata <- function(end_point, base_url = getOption("baseurl"),
   }
   #filter block
   if (!(is.null(filters))) {
-    #takes filter argument and turns it into a single character string
-    ex <- stringr::str_flatten(unlist(sapply(filters, as.character)))
-    #removes extraneous info
-    look <- sub("\\?filter=", "", ex)
-    look <- sub("\\&filter=", "", ex)
-    #extracts end_point and what is not end_point
-    if (!(grepl("^id|name", look))) {
-      end_point <- stringr::str_extract(look, ".+?(?=id|name)")
-      look <- sub("(.*)(\\id|name)", "\\2", look)
-      end_point <- gsub("/", "", end_point)
-    }
-    #extracts the original filter 
-    filter_option_orig <-
-      stringr::str_extract(substr(look, 1, 10),
-                           "!ilike|!like|ilike|!in|!eq|in|eq|like")
-    #extracts either id or name from filter
-    filter_item <- stringr::str_extract(substr(look, 1, 10), "id|name")
-    #this block replaces the filter with one more adequate (eq=in, like=ilike, etc.)
-    if (grepl("eq", filter_option_orig)) {
-      filter_option <- sub("eq", "in", filter_option_orig)
-    } else if ("like" == filter_option_orig) {
-      filter_option <- sub("like", "ilike", filter_option_orig)
-    } else {
-      filter_option <- filter_option_orig
-    }
-    #creates a basic filter path
-    ex <- paste0(gsub(filter_option_orig, filter_option, substr(look, 1, 5)),
-                 substr(look, 6, nchar(look)))
-    #removes :
-    ex <- gsub(":", "", ex)
-    #takes first part of filter, i.e. idin
-    one <- sub(paste0("(", filter_item, filter_option, ")", "(.*)"),
-               "\\1", ex)
-    one.one <- sub(paste0("(", filter_item, filter_option, ")", "(.*)"),
-                   "\\2", ex)
-    #takes second part of filter, i.e. ["abc"] and adds commas if filter = in
-    if(grepl("name",one)){
-      two <- gsub("(\\w)([A-Z])", "\\1,\\2",
-                one.one, perl = TRUE)
-    }else{
-    two <- ifelse(grepl(",", one.one), one.one,
-                  gsub("(.{11})", "\\1,",
-                       one.one, perl = TRUE))}
-    ex <- paste0(end_point, one, two)
-    if (substr(ex, nchar(ex), nchar(ex)) == ",") {
-      ex <- substr(ex, 1, nchar(ex) - 1)
-    }
-    #adds &filter= where needed, and : where needed
-    ex <- sub(paste0("(.*?)(\\&filter=|", filter_item, "|?filter=", ")"),
-              paste0("\\1&filter=", filter_item), ex)
-    ex <- sub(paste0("(.*?)", "(", filter_item, ")"),
-              paste0("\\1", filter_item, ":"), ex)
-    ex <- sub(paste0("(.*?)", "(", filter_option, ")"),
-              paste0("\\1", filter_option, ":"), ex)
-    #special processing for "in" filter
-    if (grepl("in", filter_option) & !(grepl("\\[", ex))) {
-      ex <- paste0(sub("(.*?)(in:)", "\\1in:[", ex), "]")
-    }
-    #removes whitespace
-    ex <- gsub(" ", "", ex)
+    ex <- processFilters(filters)
   }
   if (!(is.null(fields))) {
     #flattens fields and adds ?fields= if needed
@@ -92,8 +106,16 @@ getMetadata <- function(end_point, base_url = getOption("baseurl"),
       ef <- paste0("&fields=", ef)
     }
   }
+  
+  #check if there are other filter arguments than just the firtst filter and process them
+  if(!(is.null(filters2))) {
+    ex2 <- processFilters(filters2)
+    ex2 <- sub(end_point, "", ex2)
+  }
+  
   #create final path
-  path <- paste0(ex, ef, ifelse(pluck, "~pluck", ""))
+  path <- paste0(ex, ifelse(is.null(filters2), "", ex2), ef,
+                 ifelse(pluck, "~pluck", ""))
   if (is.null(fields) & is.null(filters)) {
     path <- end_point
   }
--- R/getOrgUnitGroups.R
+++ R/getOrgUnitGroups.R
@@ -1,67 +1,17 @@
-# "api/organisationUnitGroups/cNzfcPWEGSH.csv?fields=organisationUnits[name,id,path]"
-# countries <-
-#   datapackr::api_call("organisationUnits") %>%
-#   datapackr::api_filter(field = "organisationUnitGroups.id",
-#                         operation = "eq",
-#                         match = "cNzfcPWEGSH") %>%
-#   datapackr::api_fields(fields = "id,name,level,ancestors[id,name]") %>%
-#   datapackr::api_get() %>%
-#   
-#   dplyr::filter(
-#     !name %in% 
-#       c("Antigua & Barbuda","Bahamas","Belize","China","Dominica","Grenada",
-#         "Saint Kitts & Nevis","Saint Lucia","Saint Vincent & the Grenadines",
-#         "Turkmenistan","Uzbekistan")) %>%
-#   dplyr::select(country_name = name, country_uid = id, dplyr::everything()) %>%
-#   
-#   
-#   countries <-
-#   datapackr::api_call("organisationUnits") %>%
-#   datapackr::api_filter("organisationUnitGroups.name:eq:Country") %>%
-#   datapackr::api_filter(paste0("children.id:in:[",site_uids,"]")) %>%
-#   datapackr::api_fields("id,name") %>%
-#   datapackr::api_get()
-# 
-# dplyr::if_else(include_mil, ",nwQbMeALRjL", ""),
-# "]")) %>%
-#   datapackr::api_fields("id,name,level,ancestors[id,name],organisationUnitGroups[id,name]") %>%
-#   datapackr::api_get() %>%
-#   dplyr::mutate(
-#     country_uid =
-#       stringr::str_extract(as.character(ancestors),
-#                            paste0(country_uids,collapse = "|")))
-# 
-# 
-# &filter=name:$ilike:_Military",
-# #"&filter=organisationUnitGroups.id:eq:nwQbMeALRjL", (New _Mil nodes not here...)
-# "&fields=name,id,level,ancestors[id,name]") 
-# 
-# 
-# getOrgUnitGroups(c("cNzfcPWEGSH", "nwQbMeALRjL"), fields = "name,id,organisationUnits[name,id,path]" )
-# - default is to use ids for the primary filter
-# - use in for the filter as eq is a subset of in
-# - Should we unnest somehow, if so probably have a parameter for unnest with a default of TRUE
-# - baseurl/api/organisationUnitGroups.json?paging=false&filter=id:in:[cNzfcPWEGSH,nwQbMeALRjL]&fields=name,id,organisationUnits[name,id,path]
-# 
-# getOrgUnitGroups(c("Country", "Military"), by = "name", fields = "name,id,organisationUnits[name,id,path]" )
-# - filtering on name instead of ID so we add by prameter
-# - baseurl/api/organisationUnitGroups.json?paging=false&filter=name:in:[Country,Military]&fields=name,id,organisationUnits[name,id,path]
-# 
-# 
-# getOrgUnitGroups2(.x = c("cNzfcPWEGSH", "nwQbMeALRjL"), .y = c("Military"), .x_by = "id", .y_by = "name")
-# - This one will have two filters, we don't seem to have a use case for more filters than that right now, and one can always further filter in their code
-# - use .x and .y as input parameters as in purrr:map2
-# - default fields are name and ID
-# - should we still assume .x_by has a default of id?
-# - baseurl/api/organisationUnitGroups.json?paging=false&filter=id:in:[cNzfcPWEGSH,nwQbMeALRjL]&filter=name:in:[Military]&fields=name,id
-# 
-
-
-
-getOrgUnitGroups(c("cNzfcPWEGSH", "nwQbMeALRjL"))
-getOrgUnitGroups(c("Country", "Military"), by = "name", fields = "name,id,organisationUnits[name,id,path]" )
-filters = c("name", "in", c("Country", "Military"))
-getOrgUnitGroups <- function(filters = NULL, fields = NULL, base_url = "http://www.datim.org/", by = NULL)
+#' @export
+#' @title getOrgUnitGroups(filters = NULL, fields = NULL, base_url = NULL, by = NULL)
+#'
+#' @description wrapper to getMetadata that retrieves org units
+#' @param base_url string - base address of instance (text before api/ in URL)
+#' @param filters - the filters, which can come in any format as long as all
+#' components are present
+#' @param fields - the fields, which can come in any formt as long as all
+#' components are present
+#' @param by - what to filter by, i.e. id or name, default is id
+#' @return the metadata response in json format and flattened
+#'
+#'
+getOrgUnitGroups <- function(filters = NULL, fields = NULL, base_url = NULL, by = NULL)
 {
   
   default_feilds <- ifelse(is.null(fields),c("name","id"), fields)
@@ -72,5 +22,38 @@ getOrgUnitGroups <- function(filters = NULL, fields = NULL, base_url = "http://w
 }
 
 
-# - baseurl/api/organisationUnitGroups.json?paging=false&filter=name:in:[Country,Military]&fields=name,id,organisationUnits[name,id,path]
-# 
+#' @export
+#' @title getOrgUnitGroups2(filters1 = NULL, filters2 = NULL, fields = NULL, 
+#' base_url = NULL, by1 = NULL, by2 = NULL)
+#' @description wrapper to getMetadata that retrieves org units
+#' @param base_url string - base address of instance (text before api/ in URL)
+#' @param filters1 - the filters, which can come in any format as long as all
+#' components are present
+#' @param filters2 - the filters, which can come in any format as long as all
+#' components are present
+#' @param fields - the fields, which can come in any formt as long as all
+#' components are present
+#' @param by1 - what to filter by, i.e. id or name, default is id, applies to filter1
+#' @param by2 - what to filter by, i.e. id or name, default is id, applies to filter2
+#' @return the metadata response in json format and flattened
+#'
+#'
+getOrgUnitGroups2 <- function(filters1 = NULL, filters2 = NULL, by1 = NULL, by2 = NULL,
+                        fields = NULL, base_url = "https://www.datim.org/")
+{
+  
+  default_feilds <- if(is.null(fields)){c("name","id")}else{ fields}
+  default_filter_item1 = ifelse(is.null(by1),"id", by1)
+  default_filter_option1 = "in"
+  default_filter_item2 = ifelse(is.null(by2),"id", by2)
+  default_filter_option2 = "in"
+  getMetadata(base_url = base_url, end_point = "organisationUnitGroups", 
+              filters = c(default_filter_item1, default_filter_option1,filters1), 
+              fields = default_feilds, filters2 = c(default_filter_item2, 
+              default_filter_option2,filters2) )
+}
+
+
+
+
+
