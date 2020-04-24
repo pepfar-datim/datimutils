ba20a415055d784a3482a41b593199791fb6decc
sam-bao <sgarman@baosystems.com>
2020-04-24 08:36:53 +0200

    Clean up mock creation

--- tests/testthat/test-api_get.R
+++ tests/testthat/test-api_get.R
@@ -1,26 +1,20 @@
 context("make arbitrary api call DATIM")
 
 code_used_to_generate_mock_requests <- function() {
+  
   library(httptest)
+  
   httptest::start_capturing(simplify = FALSE)
   #not logged in for this one
-  datimutils::api_get(path = "api/me?fields=notloggedin",
-          base_url = "https://play.dhis2.org/2.33/",
-          retry = 1,
-          timeout = 60,
-          api_version = NULL)
+  httr::GET("https://play.dhis2.org/2.33/api/me.json?paging=false&fields=notloggedin")
   httptest::stop_capturing()
+  
   datapackcommons::DHISLogin_Play("2.33")
+
   httptest::start_capturing(simplify = FALSE)
-    datimutils::api_get(path = "apii/me", 
-          base_url = "https://play.dhis2.org/2.33/",
-          retry = 1, timeout = 60,
-          api_version = NULL
-          )
-    datimutils::api_get(path = "organisationUnits?timeout",
-            base_url =  "https://play.dhis2.org/2.33/",
-            timeout = .001)
-    httptest::stop_capturing()
+  httr::GET("https://play.dhis2.org/2.33/apii/me.json?paging=false")
+  httr::GET("https://play.dhis2.org/2.33/api/me.json?paging=false")
+  httptest::stop_capturing()
   }
 
  httptest::with_mock_api({
@@ -46,7 +40,6 @@ code_used_to_generate_mock_requests <- function() {
    })
  
  test_that("We can issue basic api calls", {
-   datapackcommons::DHISLogin_Play("2.33")
 
    user <- api_get(path = "api/me",
                    base_url =  "https://play.dhis2.org/2.33/")
@@ -70,7 +63,6 @@ code_used_to_generate_mock_requests <- function() {
  })
  
  test_that("Can use extra parameters", {
-   datapackcommons::DHISLogin_Play("2.33")
    skip_if_disconnected()
    #timeout should be short enough to trip this but an internet connection is required
    expect_error(api_get(path = "organisationUnits?timeout",
