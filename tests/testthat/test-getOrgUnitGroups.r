534055326b5b48b4362c3b15b776488a18c5a87c
sam-bao <sgarman@baosystems.com>
2020-05-14 14:10:28 +0200

    Update test-getOrgUnitGroups.r
    more unit tests.

--- tests/testthat/test-getOrgUnitGroups.r
+++ tests/testthat/test-getOrgUnitGroups.r
@@ -1,11 +1,9 @@
 context("make arbitrary api call with getorgunitgroups")
 
 # code to create/update mocks
-# httptest::start_capturing(simplify = FALSE)
-# #not logged in for this one, fields there just to give the call a unique httptest ID
-# httr::GET("https://play.dhis2.org/2.33/api/me.json?paging=false&fields=notloggedin")
-# httptest::stop_capturing()
+library(httptest)
 
+#httptest::start_capturing(simplify = FALSE)
 httr::content(
   httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
                  "paging=false&filter=id:in:[CXw2yu5fodb]&fields=name,id"),
@@ -28,21 +26,66 @@ datimutils::getOrgUnitGroups(c("CHP", "Rural"),
 httr::content(
   httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
                    "paging=false&filter=name:in:[CHP,Rural]",
-                   "&fields=name,id,organisationUnits[name,id]"),
+                   "&fields=name,id,organisationUnits[name,id],groupSets[name,id]"),
             httr::authenticate("admin", "district"))
 )
-datimutils::getOrgUnitGroups(c("CHP", "Rural"),
-                             by = "name",
-                             fields = "name,id,organisationUnits[name,id]",
-                             base_url = "https://play.dhis2.org/2.33/")
-# httptest::start_capturing(simplify = FALSE)
+datimutils::getOrgUnitGroups(
+  c("CHP", "Rural"),
+  by = "name",
+  fields = "name,id,organisationUnits[name,id],groupSets[name,id]",
+  base_url = "https://play.dhis2.org/2.33/")
 
-with_mock_api({
-  test_that("We can get a orgunit response", {
-    see <- getOrgUnitGroups(x = c("Country", "Military"),
-                            by = "name",
-                            fields = "name,id,organisationUnits[name,id,path]",
-                            base_url = "www.datim.org/")
-    expect_identical(see$organisationUnitGroups$name, "Country")
-  })
+#httptest::stop_capturing()
+
+test_that(
+  paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+         "paging=false&filter=id:in:[CXw2yu5fodb]&fields=name,id"), {
+           
+           data <- datimutils::getOrgUnitGroups("CXw2yu5fodb", 
+                                        base_url = "https://play.dhis2.org/2.33/")
+           #data <- tibble::as_tibble(data$organisationUnitGroups)
+           testthat::expect_s3_class(data, "data.frame")
+           testthat::expect_equal(NROW(data), 1)
+           testthat::expect_named(data, c("name", "id"))
+           testthat::expect_equal(data$id, "CXw2yu5fodb")
 })
+
+test_that(
+  paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+         "paging=false&filter=name:in:[CHP,Rural]",
+         "&fields=id,code"), {
+           
+           data <- 
+             datimutils::getOrgUnitGroups(
+               c("CHP", "Rural"),
+               by = "name",
+               fields = c("id", "code"),
+               base_url = "https://play.dhis2.org/2.33/"
+               )
+           
+           #data <- tibble::as_tibble(data$organisationUnitGroups)
+           testthat::expect_equal(NROW(data), 2)
+           testthat::expect_named(data, c("code", "id"))
+           #testthat::expect_equal(data$id, "CXw2yu5fodb")
+         })
+
+test_that(
+  paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+         "paging=false&filter=name:in:[CHP,Rural]",
+         "&fields=name,id,organisationUnits[name,id],groupSets[name,id]"), {
+           
+           data <-
+             datimutils::getOrgUnitGroups(
+               c("CHP", "Rural"),
+               by = "name",
+               fields = c("name", "id", "organisationUnits[name,id]",
+                          "groupSets[name,id]"),
+               base_url = "https://play.dhis2.org/2.33/"
+             )
+           
+           #data <- tibble::as_tibble(data$organisationUnitGroups)
+           testthat::expect_s3_class(data, "data.frame")
+           testthat::expect_equal(NROW(data), 2)
+           #testthat::expect_named(data, c("code", "id"))
+           #testthat::expect_equal(data$id, "CXw2yu5fodb")
+         })
