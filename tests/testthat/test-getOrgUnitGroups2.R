6a3c47e455b87343f88a9f87c2e98c9d4653c64c
sam-bao <sgarman@baosystems.com>
2020-05-20 15:03:37 +0200

    adding additional mocks

--- tests/testthat/test-getOrgUnitGroups2.R
+++ tests/testthat/test-getOrgUnitGroups2.R
@@ -4,20 +4,12 @@ context("make arbitrary api call with getorgunitgroups2")
 library(httptest)
 
 #httptest::start_capturing(simplify = FALSE)
-
-httr::content(
-  httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
-                 "paging=false&filter=id:in:[CXw2yu5fodb,gzcv65VyaGq]",
-                 "&filter=groupSets.id:in:[J5jldMd8OHv]&fields=code"),
-          httr::authenticate("admin", "district"))
-)
-datimutils::getOrgUnitGroups2(c("CXw2yu5fodb","gzcv65VyaGq"),
-                              "J5jldMd8OHv",
-                              "id",
-                              "groupSets.id",
-                              c("code"),
-                              "https://play.dhis2.org/2.33/")
-
+# httr::content(
+#   httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+#                  "paging=false&filter=id:in:[CXw2yu5fodb,gzcv65VyaGq]",
+#                  "&filter=groupSets.id:in:[J5jldMd8OHv]&fields=code"),
+#           httr::authenticate("admin", "district"))
+# )
 #httptest::stop_capturing()
 
 test_that(
