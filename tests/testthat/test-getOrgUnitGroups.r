f59d45577fd81e52e79208bbd87850494b701cc7
sam-bao <sgarman@baosystems.com>
2020-05-20 13:24:38 +0200

    adding and using mocks

--- tests/testthat/test-getOrgUnitGroups.r
+++ tests/testthat/test-getOrgUnitGroups.r
@@ -4,54 +4,54 @@ context("make arbitrary api call with getorgunitgroups")
 library(httptest)
 
 #httptest::start_capturing(simplify = FALSE)
-httr::content(
-  httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
-                 "paging=false&filter=id:in:[CXw2yu5fodb]&fields=name,id"),
-          httr::authenticate("admin", "district"))
-)
-datimutils::getOrgUnitGroups("CXw2yu5fodb", 
-                             base_url = "https://play.dhis2.org/2.33/")
-
-httr::content(
-  httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
-                 "paging=false&filter=name:in:[CHP,Rural]",
-                 "&fields=id,code"),
-          httr::authenticate("admin", "district"))
-)
-datimutils::getOrgUnitGroups(c("CHP", "Rural"),
-                             by = "name",
-                             fields = c("id", "code"),
-                             base_url = "https://play.dhis2.org/2.33/")
-
-httr::content(
-  httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
-                   "paging=false&filter=name:in:[CHP,Rural]",
-                   "&fields=name,id,organisationUnits[name,id],groupSets[name,id]"),
-            httr::authenticate("admin", "district"))
-)
-datimutils::getOrgUnitGroups(
-  c("CHP", "Rural"),
-  by = "name",
-  fields = "name,id,organisationUnits[name,id],groupSets[name,id]",
-  base_url = "https://play.dhis2.org/2.33/")
-
+# httr::content(
+#   httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+#                  "paging=false&filter=id:in:[CXw2yu5fodb]&fields=name,id"),
+#           httr::authenticate("admin", "district"))
+# )
+# datimutils::getOrgUnitGroups("CXw2yu5fodb", 
+#                              base_url = "https://play.dhis2.org/2.33/")
+# 
+# httr::content(
+#   httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+#                  "paging=false&filter=name:in:[CHP,Rural]",
+#                  "&fields=id,code"),
+#           httr::authenticate("admin", "district"))
+# )
+# datimutils::getOrgUnitGroups(c("CHP", "Rural"),
+#                              by = "name",
+#                              fields = c("id", "code"),
+#                              base_url = "https://play.dhis2.org/2.33/")
+# 
+# httr::content(
+#   httr::GET(paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+#                    "paging=false&filter=name:in:[CHP,Rural]",
+#                    "&fields=name,id,organisationUnits[name,id],groupSets[name,id]"),
+#             httr::authenticate("admin", "district"))
+# )
+# datimutils::getOrgUnitGroups(
+#   c("CHP", "Rural"),
+#   by = "name",
+#   fields = "name,id,organisationUnits[name,id],groupSets[name,id]",
+#   base_url = "https://play.dhis2.org/2.33/")
 #httptest::stop_capturing()
 
-test_that(
-  paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
-         "paging=false&filter=id:in:[CXw2yu5fodb]&fields=name,id"), {
-           
-           data <- datimutils::getOrgUnitGroups("CXw2yu5fodb", 
-                                        base_url = "https://play.dhis2.org/2.33/")
-           #data <- tibble::as_tibble(data$organisationUnitGroups)
-           testthat::expect_s3_class(data, "data.frame")
-           testthat::expect_equal(NROW(data), 1)
-           testthat::expect_named(data, c("name", "id"))
-           testthat::expect_equal(data$id, "CXw2yu5fodb")
-           rm(data)
-})
-
-test_that("Uses default base_url", {
+httptest::with_mock_api({
+  test_that(
+    paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+           "paging=false&filter=id:in:[CXw2yu5fodb]&fields=name,id"), {
+             
+             data <- datimutils::getOrgUnitGroups("CXw2yu5fodb", 
+                                                  base_url = "https://play.dhis2.org/2.33/")
+             #data <- tibble::as_tibble(data$organisationUnitGroups)
+             testthat::expect_s3_class(data, "data.frame")
+             testthat::expect_equal(NROW(data), 1)
+             testthat::expect_named(data, c("name", "id"))
+             testthat::expect_equal(data$id, "CXw2yu5fodb")
+             rm(data)
+           })
+  
+  test_that("Uses default base_url", {
     original_baseurl = getOption("baseurl")
     options("baseurl" = "https://play.dhis2.org/2.33/")
     data <- datimutils::getOrgUnitGroups("CXw2yu5fodb")
@@ -59,63 +59,64 @@ test_that("Uses default base_url", {
     options("baseurl" = original_baseurl)
     rm(data)
   })
-
-test_that(
-  paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
-         "paging=false&filter=name:in:[CHP,Rural]",
-         "&fields=id,code"), {
-           
-           data <- 
-             datimutils::getOrgUnitGroups(
-               c("CHP", "Rural"),
-               by = "name",
-               fields = c("id", "code"),
-               base_url = "https://play.dhis2.org/2.33/"
+  
+  test_that(
+    paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+           "paging=false&filter=name:in:[CHP,Rural]",
+           "&fields=id,code"), {
+             
+             data <- 
+               datimutils::getOrgUnitGroups(
+                 c("CHP", "Rural"),
+                 by = "name",
+                 fields = c("id", "code"),
+                 base_url = "https://play.dhis2.org/2.33/"
                )
-           
-           #data <- tibble::as_tibble(data$organisationUnitGroups)
-           testthat::expect_equal(NROW(data), 2)
-           testthat::expect_named(data, c("code", "id"))
-           #testthat::expect_equal(data$id, "CXw2yu5fodb")
-           rm(data)
-         })
-
-test_that(
-  paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
-         "paging=false&filter=name:in:[CHP,Rural]",
-         "&fields=name,id,organisationUnits[name,id],groupSets[name,id]"), {
-           
-           data <-
-             datimutils::getOrgUnitGroups(
-               c("CHP", "Rural"),
-               by = "name",
-               fields = c("name", "id", "organisationUnits[name,id]",
-                          "groupSets[name,id]"),
-               base_url = "https://play.dhis2.org/2.33/"
-             )
-           
-           #data <- tibble::as_tibble(data$organisationUnitGroups)
-           testthat::expect_s3_class(data, "data.frame")
-           testthat::expect_equal(NROW(data), 2)
-           #testthat::expect_named(data, c("code", "id"))
-           #testthat::expect_equal(data$id, "CXw2yu5fodb")
-           rm(data)
-         })
-
-test_that(
-  paste0("getOrgUnitGroups can handle a large input list"), {
-    groups <- datimutils::getMetadata("organisationUnitGroups",
-                                      base_url = "https://play.dhis2.org/2.33/",
-                                      wrapper_reduce = "organisationUnitGroups"
-    )
-    groups <- groups$id
-    groups <- rep(groups, 100)
-    data <-
-      datimutils::getOrgUnitGroups(groups, fields = "id",
-                                   base_url = "https://play.dhis2.org/2.33/"
+             
+             #data <- tibble::as_tibble(data$organisationUnitGroups)
+             testthat::expect_equal(NROW(data), 2)
+             testthat::expect_named(data, c("code", "id"))
+             #testthat::expect_equal(data$id, "CXw2yu5fodb")
+             rm(data)
+           })
+  
+  test_that(
+    paste0("https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+           "paging=false&filter=name:in:[CHP,Rural]",
+           "&fields=name,id,organisationUnits[name,id],groupSets[name,id]"), {
+             
+             data <-
+               datimutils::getOrgUnitGroups(
+                 c("CHP", "Rural"),
+                 by = "name",
+                 fields = c("name", "id", "organisationUnits[name,id]",
+                            "groupSets[name,id]"),
+                 base_url = "https://play.dhis2.org/2.33/"
+               )
+             
+             #data <- tibble::as_tibble(data$organisationUnitGroups)
+             testthat::expect_s3_class(data, "data.frame")
+             testthat::expect_equal(NROW(data), 2)
+             #testthat::expect_named(data, c("code", "id"))
+             #testthat::expect_equal(data$id, "CXw2yu5fodb")
+             rm(data)
+           })
+})  
+  test_that(
+    paste0("getOrgUnitGroups can handle a large input list"), {
+      groups <- datimutils::getMetadata("organisationUnitGroups",
+                                        base_url = "https://play.dhis2.org/2.33/",
+                                        wrapper_reduce = "organisationUnitGroups"
       )
-    #data <- tibble::as_tibble(data$organisationUnitGroups)
-    testthat::expect_equal(NROW(data), 1800)
-    testthat::expect_identical(groups, data$id)
-    rm(data)
-         })
+      groups <- groups$id
+      groups <- rep(groups, 100)
+      data <-
+        datimutils::getOrgUnitGroups(groups, fields = "id",
+                                     base_url = "https://play.dhis2.org/2.33/"
+        )
+      #data <- tibble::as_tibble(data$organisationUnitGroups)
+      testthat::expect_equal(NROW(data), 1800)
+      testthat::expect_identical(groups, data$id)
+      rm(data)
+    })
+
