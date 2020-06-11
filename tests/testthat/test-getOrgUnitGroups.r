fc5f38b0e569fe707995adccf72b92b6633b63dd
sam-bao <sgarman@baosystems.com>
2020-06-11 17:18:08 +0200

    Fix an incorrect test and add mocks.

--- tests/testthat/test-getOrgUnitGroups.r
+++ tests/testthat/test-getOrgUnitGroups.r
@@ -85,20 +85,24 @@ httptest::with_mock_api({
 
   test_that(
     paste0("Default behavior, if provide filter property other than name or ", 
-           "id then name and id returned by default: "), {
+           "id then name returned by default: "), {
   
 # httr::content(httr::GET(
 #   paste0(
 #          "https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
 #          "paging=false&filter=code:in:[CHC]&fields=name,id")))
+httr::content(httr::GET(
+  paste0(
+         "https://play.dhis2.org/2.33/api/organisationUnitGroups.json?",
+         "paging=false&filter=code:in:[CHC]&fields=name")))
              
-             data <- getOrgUnitGroups(
+             sdata <- getOrgUnitGroups(
                "CHC", by = code,
                base_url = "https://play.dhis2.org/2.33/"
              )
              
              testthat::expect_equal(NROW(data), 1)
-             testthat::expect_named(data, c("name", "id"))
+             testthat::expect_named(data, c("name"))
              rm(data)
            }
   )
