1351e710799bc56a552b5b4bf851e4e26d896f9c
maxwellchandler <mchandler@baosystems.com>
2020-08-18 09:11:16 -0600

    added more tests for getAnalytics.R and also new functionality

--- R/getAnalytics.R
+++ R/getAnalytics.R
@@ -1,6 +1,6 @@
 #' @export
 #' @title getAnalytics
-#' @description calls the analytics endpoint u
+#' @description calls the analytics endpoint
 #' @param ... any options here to tag on the end of the url
 #' @param dimensions dimensions
 #' @param filters filters
@@ -9,30 +9,33 @@
 #' @param order order
 #' @param columns columns
 #' @param rows rows
-#' @param sort_order sort_order
 #' @param base_url string - base address of instance (text before api/ in URL)
 #' @param retry retry
 #' @return data frame with the rows of the response
 
 getAnalytics <-  function(..., dimensions = NULL, filters = NULL, start_date = NULL, end_date = NULL,
-                               order = NULL, columns = NULL, rows = NULL, sort_order = NULL,
+                               order = NULL, columns = NULL, rows = NULL,
                                base_url = getOption("baseurl"), retry=1){
+  #variable set-up
   table <- NULL
+  start <- F
 
-  if(!(is.null(sort_order)))
-    {
-    sort_order <- paste0("sortOrder=", sort_order)
-  }
+  #check if named arguments are passed in and format them
   if(!(is.null(order)))
     {
-    order <- paste0("order=", sort_order)
+    order <- paste0("order=", order)
   }
   if(!(is.null(start_date)))
     {
+    start <- T
     start_date <- paste0("startDate=", start_date)
   }
   if(!(is.null(end_date)))
     {
+    if(start)
+      {
+      stop("Periods and start and end dates cannot be specified simultaneously")
+    }
     end_date <- paste0("endDate=", end_date)
   }
       if(!(is.null(rows)) & !(is.null(columns)))
@@ -40,13 +43,17 @@ getAnalytics <-  function(..., dimensions = NULL, filters = NULL, start_date = N
     table <- paste0("tableLayout=true&columns=",
                     paste0(columns, collapse = ";"),"&rows=",
                     paste0(rows, collapse = ";"))
-  }
+  } else if((is.null(rows) & !(is.null(columns))) | (is.null(columns) & !(is.null(rows)))){
+        stop("To widen data both columns and rows arguments have to be present")
+      }
 
+  #process ...
   end_point <- "analytics?"
   ends <- unlist(list(...))
   ends <- paste0(ends,collapse = "&")
 
-  path <- paste0(end_point, stringr::str_c(dimensions, filters,table, sort_order, order, start_date, end_date,  ends,  sep = "&"))
+  #collapse everything and form path
+  path <- paste0(end_point, stringr::str_c(dimensions, filters, table, order, start_date, end_date, ends, sep = "&"))
   if(substr(path,nchar(path),nchar(path)) == "&")
     {
     path <- substr(path,1,nchar(path)-1)
@@ -55,16 +62,24 @@ getAnalytics <-  function(..., dimensions = NULL, filters = NULL, start_date = N
     path = path, base_url = base_url, retry = retry
   )
 
+  #collect data types
+  coercions <- resp$headers$valueType
+  #collect replacements for uids
   replacements <- resp$metaData$items
-  col_names <- resp$headers$name
+  #collect column names
+  col_names <- resp$headers$column
+  #get data
   resp <- as.data.frame(resp$rows)
+  #change column names
   colnames(resp) <- col_names
-
+  #replace uids with readable names
   resp <- apply(resp,2, function(x) {ifelse(x %in% names(replacements),
          unlist(replacements[names(replacements) %in% x]),
          x)})
 
-  resp <- as.data.frame(resp)
+  resp <- as.data.frame(resp, stringsAsFactors = F)
+  #change data types to numeric where possible
+  resp[,coercions == "NUMBER"] <- sapply(resp[,coercions == "NUMBER"], as.numeric)
 
   return(resp)
 }
--- tests/testthat/test-getAnalytics.R
+++ tests/testthat/test-getAnalytics.R
@@ -4,20 +4,20 @@
 
 with_mock_api({
 
-test_that("Basic eq and sinlge element in call: ", {
+test_that("all arguments in getAnalytics function: ", {
 #httr::content(httr::GET(
-#"https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=dx:fbfJHSPpUQD;cYeuwXTCPkU&dimension=pe:THIS_YEAR;LAST_YEAR&dimension=ou:ImspTQPwCqd;LEVEL-4&displayProperty=NAME&hierarchyMeta=true&outputIdScheme=UID"))
+#"https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=Data:fbfJHSPpUQD;cYeuwXTCPkU&dimension=pe:THIS_YEAR;LAST_YEAR&dimension=ou:ImspTQPwCqd;LEVEL-4&displayProperty=NAME&hierarchyMeta=true&outputIdScheme=UID"))
 
 dimensions <- dForm(c("fbfJHSPpUQD","cYeuwXTCPkU"), c("THIS_YEAR","LAST_YEAR"), c("ImspTQPwCqd","LEVEL-4"), id = c("dx","pe", "ou"))
 data <- getAnalytics("displayProperty=NAME", "hierarchyMeta=true", "outputIdScheme=UID", dimensions = dimensions, base_url = "https://play.dhis2.org/2.33.5/")
 
 testthat::expect_s3_class(data, "data.frame")
 testthat::expect_equal(NROW(data), 4394)
-testthat::expect_named(data, c("dx","pe","ou","value"))
+testthat::expect_named(data, c("Data","Period","Organisation unit","Value"))
 rm(data) })
 test_that("Basic eq and sinlge element in call: ", {
 #httr::content(httr::GET(
-#"https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=dx:fbfJHSPpUQD.pq2XI5kz2BY;fbfJHSPpUQD.PT59n8BQbqM;cYeuwXTCPkU.pq2XI5kz2BY;cYeuwXTCPkU.PT59n8BQbqM;o15CyZiTvxa;f27B1G7B3m3;hJNC4Bu2Mkv&dimension=pe:201908;201909;201910;201911;201912;202001;202002;202003;202004;202005;202006;202007&filter=ou:ImspTQPwCqd&displayProperty=NAME&outputIdScheme=NAME"))
+#"https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=Data:fbfJHSPpUQD.pq2XI5kz2BY;fbfJHSPpUQD.PT59n8BQbqM;cYeuwXTCPkU.pq2XI5kz2BY;cYeuwXTCPkU.PT59n8BQbqM;o15CyZiTvxa;f27B1G7B3m3;hJNC4Bu2Mkv&dimension=pe:201908;201909;201910;201911;201912;202001;202002;202003;202004;202005;202006;202007&filter=ou:ImspTQPwCqd&displayProperty=NAME&outputIdScheme=NAME"))
 dimensions <- dForm(c("fbfJHSPpUQD.pq2XI5kz2BY","fbfJHSPpUQD.PT59n8BQbqM","cYeuwXTCPkU.pq2XI5kz2BY","cYeuwXTCPkU.PT59n8BQbqM","o15CyZiTvxa","f27B1G7B3m3","hJNC4Bu2Mkv"),
 c("201908","201909","201910","201911","201912","202001","202002","202003","202004","202005","202006","202007"), id = c("dx","pe"))
 filters <- fForm("ImspTQPwCqd", id = "ou")
@@ -25,10 +25,44 @@ data <- getAnalytics("displayProperty=NAME","outputIdScheme=NAME", dimensions = 
 
 testthat::expect_s3_class(data, "data.frame")
 testthat::expect_equal(NROW(data), 84)
-testthat::expect_named(data, c("dx","pe","value"))
+testthat::expect_named(data, c("Data","Period","Value"))
 rm(data)
+#  httr::content(httr::GET(
+#"https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=dx:fbfJHSPpUQD.pq2XI5kz2BY;fbfJHSPpUQD.PT59n8BQbqM;cYeuwXTCPkU.pq2XI5kz2BY;cYeuwXTCPkU.PT59n8BQbqM;o15CyZiTvxa;f27B1G7B3m3;hJNC4Bu2Mkv&dimension=pe:201908;201909;201910;201911;201912;202001;202002;202003;202004;202005;202006;202007&filter=ou:ImspTQPwCqd&startDate=2017-01-01&displayProperty=NAME&outputIdScheme=NAME"))
+data <- getAnalytics("displayProperty=NAME","outputIdScheme=NAME", dimensions = dimensions, filters = filters, start_date = "2017-01-01", base_url = "https://play.dhis2.org/2.33.5/")
+testthat::expect_s3_class(data, "data.frame")
+testthat::expect_equal(NROW(data), 84)
+testthat::expect_named(data, c("Data","Period","Value"))
+rm(data)
+#httr::content(httr::GET(
+#  "https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=dx:fbfJHSPpUQD.pq2XI5kz2BY;fbfJHSPpUQD.PT59n8BQbqM;cYeuwXTCPkU.pq2XI5kz2BY;cYeuwXTCPkU.PT59n8BQbqM;o15CyZiTvxa;f27B1G7B3m3;hJNC4Bu2Mkv&dimension=pe:201908;201909;201910;201911;201912;202001;202002;202003;202004;202005;202006;202007&filter=ou:ImspTQPwCqd&endDate=2019-01-01&displayProperty=NAME&outputIdScheme=NAME"))
+data <- getAnalytics("displayProperty=NAME","outputIdScheme=NAME", dimensions = dimensions, filters = filters, end_date = "2019-01-01", base_url = "https://play.dhis2.org/2.33.5/")
+testthat::expect_s3_class(data, "data.frame")
+testthat::expect_equal(NROW(data), 84)
+testthat::expect_named(data, c("Data","Period","Value"))
+rm(data)
+
+  testthat::expect_error( data <- getAnalytics("displayProperty=NAME","outputIdScheme=NAME", dimensions = dimensions, filters = filters, start_date = "2017-01-01", end_date = "2019-01-01", base_url = "https://play.dhis2.org/2.33.5/")
+)
+#httr::content(httr::GET(
+#"https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=dx:fbfJHSPpUQD.pq2XI5kz2BY;fbfJHSPpUQD.PT59n8BQbqM;cYeuwXTCPkU.pq2XI5kz2BY;cYeuwXTCPkU.PT59n8BQbqM;o15CyZiTvxa;f27B1G7B3m3;hJNC4Bu2Mkv&dimension=pe:201908;201909;201910;201911;201912;202001;202002;202003;202004;202005;202006;202007&filter=ou:ImspTQPwCqd&order=DESC&endDate=2019-01-01&displayProperty=NAME&outputIdScheme=NAME"))
+  data <- getAnalytics("displayProperty=NAME","outputIdScheme=NAME", dimensions = dimensions, filters = filters, end_date = "2019-01-01", order = "DESC", base_url = "https://play.dhis2.org/2.33.5/")
+testthat::expect_s3_class(data, "data.frame")
+testthat::expect_equal(NROW(data), 84)
+testthat::expect_named(data, c("Data","Period","Value"))
+testthat::expect_identical(data$Value[1], 362454.0)
+#httr::content(httr::GET(
+#  "https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=dx:fbfJHSPpUQD.pq2XI5kz2BY;fbfJHSPpUQD.PT59n8BQbqM;cYeuwXTCPkU.pq2XI5kz2BY;cYeuwXTCPkU.PT59n8BQbqM;o15CyZiTvxa;f27B1G7B3m3;hJNC4Bu2Mkv&dimension=pe:201908;201909;201910;201911;201912;202001;202002;202003;202004;202005;202006;202007&filter=ou:ImspTQPwCqd&tableLayout=true&columns=pe&rows=dx&endDate=2019-01-01&displayProperty=NAME&outputIdScheme=NAME"))
+  data <- getAnalytics("displayProperty=NAME","outputIdScheme=NAME", dimensions = dimensions, filters = filters, end_date = "2019-01-01", rows = "dx", columns = "pe", base_url = "https://play.dhis2.org/2.33.5/")
+testthat::expect_s3_class(data, "data.frame")
+testthat::expect_equal(NROW(data), 7)
+  testthat::expect_equal(NCOL(data), 16)
+
+    testthat::expect_error( data <- getAnalytics("displayProperty=NAME","outputIdScheme=NAME", dimensions = dimensions, filters = filters, rows ="pe", base_url = "https://play.dhis2.org/2.33.5/")
+)
+
 })
-test_that("Basic eq and sinlge element in call: ", {
+test_that("multiple filters: ", {
 #httr::content(httr::GET(
 #"https://play.dhis2.org/2.33.5/api/analytics.json?paging=false&dimension=dx:BfMAe6Itzgt.REPORTING_RATE;QX4ZTUbOt3a.REPORTING_RATE;eZDhcZi6FLP.REPORTING_RATE&dimension=ou:USER_ORGUNIT;USER_ORGUNIT_CHILDREN&filter=pe:LAST_52_WEEKS;LAST_12_MONTHS&displayProperty=NAME&user=xE7jOejl9FI&outputIdScheme=UID"))
 dimensions <- dForm(c("BfMAe6Itzgt.REPORTING_RATE","QX4ZTUbOt3a.REPORTING_RATE","eZDhcZi6FLP.REPORTING_RATE"), c("USER_ORGUNIT","USER_ORGUNIT_CHILDREN"), id = c("dx","ou"))
@@ -37,32 +71,11 @@ data <- getAnalytics("displayProperty=NAME","user=xE7jOejl9FI","outputIdScheme=U
 
 testthat::expect_s3_class(data, "data.frame")
 testthat::expect_equal(NROW(data), 42)
-testthat::expect_named(data, c("dx","ou","value"))
+testthat::expect_named(data, c("Data","Organisation unit","Value"))
 rm(data)
 })
 })
-#finish this test
 
-
-#
-#
-#
-###/api/33/analytics?dimension=dx:fbfJHSPpUQD&dimension=pe:2014Q1&dimension=ou:O6uvpzGd5pu
-###  &aggregationType=COUNT
-#dimensions <- dForm("fbfJHSPpUQD", "2014Q1", "O6uvpzGd5pu",   id = c("dx","pe","ou"))
-#getAnalytics("aggregationType=COUNT", dimensions = dimensions, base_url = "https://play.dhis2.org/2.33/")
-##
-###/api/33/analytics.json?dimension=dx:fbfJHSPpUQD;cYeuwXTCPkU
-###  &dimension=ou:ImspTQPwCqd&startDate=2018-01-01&endDate=2018-06-01
-#dimensions <- dForm(c("fbfJHSPpUQD","cYeuwXTCPkU"), "ImspTQPwCqd", id = c("dx","ou"))
-#getAnalytics(dimensions = dimensions, start_date = "2018-01-01",
-#             end_date = "2018-06-01", base_url = "https://play.dhis2.org/2.33/")
-##
-###/api/33/analytics.html?dimension=dx:fbfJHSPpUQD;cYeuwXTCPkU&dimension=pe:2014Q1;2014Q2
-###  &dimension=ou:O6uvpzGd5pu&tableLayout=true&columns=dx;ou&rows=pe
-#dimensions <- dForm(c("fbfJHSPpUQD","cYeuwXTCPkU"), c("2014Q1","2014Q2"), "O6uvpzGd5pu", id = c("dx","pe","ou"))
-#getAnalytics(dimensions = dimensions, rows = "pe", columns = c("dx","ou"), base_url = "https://play.dhis2.org/2.33/")
-#
 
 
 
