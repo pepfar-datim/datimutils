2c5369500d8d0810b731f842e902b9a800f20ed6
Fausto Lopez <92821116+flopez-bao@users.noreply.github.com>
2022-03-09 10:13:50 -0500

    Adp 1425 (#80)
    * add functions
    
    added three new main functions for streams access, user classification and mechanism retrieval.
    
    * vignette and tests
    
    Added a vignette and an empty sample test file.
    
    * Changes to getStreams process
    
    -eliminated getStreams function and replaced with getUserGroups in getMetadataEndpoint.R which will give us what we need.
    
    * Added new functions
    
    Added - listMyMechs
    Added - getMyStreams
    Added - getMyUserType
    Added - getUserGroups to getMetadataEndpoint
    
    * Fixed magrittr import
    
    Fixed magrittr import for listMyMechs and removed it from getMyStreams.
    
    * Fixing PR issues
    
    -Changed name of listMyMechs to listMechs
    -Passed d2_session explicitly
    -getUserType now calls getMyStreams
    -Added getUserGroup to main vignette table
    - UNIT TESTING still left
    
    * Update Introduction-to-datimutils.Rmd
    
    * fix vignette and getMyUserType
    
    Made changes to fix vignette build and getMyUserType
    
    * Update getMyUserType.Rd
    
    update documentation.
    
    * Update Securing-user-mechanism-access.Rmd
    
    * Update listMechs.R
    
    revert mech_code to code. Given the function purpose mech_ is not needed for clarity. We already leave it off of the name column
    
    * Update Securing-user-mechanism-access.Rmd
    
    update for change from mech_code to mech
    
    * Change response
    
    Changed stream response and modified user classification.
    
    * Update listMechs.R
    
    update column name for mech code.
    
    * Testing
    
    Added 100% coverage on unit tests for getMyStreams, getMyUserType and listMechs.
    
    * Delete Securing-user-mechanism-access.Rmd
    
    Get rid of vignette.
    
    * Update helper.R
    
    test build
    
    * Update .gitignore
    
    add .Rprofile to gitignore
    
    * Remove .Rprofile
    
    .Rprofile was not in .gitignore. Added to .gitignore. Made tests backward compatible with 3.6.3. Should also hold up with >4.
    
    * Added description link
    
    Added description link for implementation.
    
    * Fixed vignette issue
    
    Removed vignette.
    
    * fixed test issues
    
    devtools::check now passes as well as devtools::test.
    
    * Update DESCRIPTION
    
    * Create NEWS.md
    
    * Update DESCRIPTION
    
    * Final Checks
    
    - moved helper user group list to each test
    - renamed uid_list to user group list
    - documented test for list mechs
    
    Co-authored-by: sam-bao <sgarman@baosystems.com>

--- R/getMyUserType.R
+++ R/getMyUserType.R
@@ -1,30 +1,44 @@
 #' @export
 #' @title getMyUserType(d2_session = dynGet("d2_default_session", inherits = TRUE))
-#' @description Returns a classified user type based on accessible data streams. 
+#' @description Returns a classified user type based on accessible data streams. For an implementation
+#' example see: https://github.com/flopez-bao/shinyapps-datimutils-security-example-usgandpartners
 #' @param d2_session the d2Session object, default is "d2_default_session"
 
 getMyUserType <- function(d2_session = dynGet("d2_default_session",inherits = TRUE)) {
   
-    #pull streams and classify
-    streams <- getMyStreams(d2_session = d2_session)
+  #pull streams and classify
+  tryCatch(
+    expr = {
+      user_groups <- getUserGroups(
+        values = d2_session$me$userGroups$id,
+        d2_session = d2_session
+      )
+    },
+    error = function(e){ 
+      stop("There was an error retrieving the user group information! Make sure you are logged into DATIM.")
+    }
+  )
+  
+  # select from user_groups everything that is a data stream and needed for classification
+  streams <- user_groups[grepl("Data (.+?) access|^Global|^OU",user_groups)]
     
-    #classify user
+    # classify user
     if( length( regmatches(streams ,regexpr("OU (.+?) MOH users", streams)) )  > 0 ) {
-      return("moh user")
+      return("MOH")
     } else if ( length(regmatches(streams ,regexpr("Global users", streams)) ) > 0 ) {
-      return("global only")
+      return("Global")
     } else if ( length( regmatches(streams, regexpr("OU (.+?) Partner (.+?) users - (.+?)",streams)) ) > 0 ) {
-      return("partner only")
+      return("Partner")
     } else if ( length( regmatches(streams, regexpr("OU (.+?) Agency (.+?) users",streams)) ) > 0 ) {
-      return("agency only")
+      return("Agency")
     } else if ( length( regmatches(streams, regexpr("OU (.+?) Interagency users",streams)) ) > 0 ) {
-      return("interagency only")
+      return("Interagency")
     } else if ( length( regmatches(streams, regexpr("Global Agency (.+?) users",streams)) ) > 0 ) {
-      return("global agency")
+      return("Global Agency")
     } else if ( length( regmatches(streams, regexpr("Global Partner (.+?) users - (.+?)",streams)) ) > 0 ) {
-      return("global partner")
+      return("Global Partner")
     } else {
-      return("unclassified user")
+      return("Unclassified User")
     }
 }
 
