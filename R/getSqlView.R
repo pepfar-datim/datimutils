d16533e3d3af228c4828f5bc887495abfe657add
Jason Pickering <jason.p.pickering@gmail.com>
2022-05-25 18:09:38 +0200

    Hotfix 610 (#85)
    * Handle nested lists in SQL views
    
    * Return NULL if SQL view is empty
    
    * Added test to ensure we could fetch a sql view that did not have a nested list
    
    * Added test to addres SQLviews that return 0 rows. Also added mock api calls.
    
    Co-authored-by: Jordan <Jordan>

--- R/getSqlView.R
+++ R/getSqlView.R
@@ -57,16 +57,26 @@ getSqlView <- function(..., sql_view_uid, variable_keys = NULL,
 
   x <- resp$listGrid$headers$name
 
-  resp <- as.data.frame(do.call("rbind",resp$listGrid$rows),
-                        stringsAsFactors = FALSE)
+  if (length(resp$listGrid$rows) == 0) {
+    return(NULL)
+  }
+  
+  #Some SQL views can have nested lists as columns.
+  has_nested_lists <-
+    any(Reduce("|", lapply(resp$listGrid$rows, function(x)
+      lapply(x, class) == "list")))
+  
+  if (has_nested_lists) {
+    resp <- as.data.frame(do.call("rbind",resp$listGrid$rows),
+                  stringsAsFactors = FALSE)
+  } else {
+    resp <- as.data.frame(resp$listGrid$rows,stringsAsFactors = FALSE)
+  }
+
   colnames(resp) <- x
 
   return(resp)
 }
-
-#?filter=orgunit_level:eq:2&filter=orgunit_name:ilike:bo
-#?criteria=level:4
-
 
 #' @export
 #' @title listSqlViews
