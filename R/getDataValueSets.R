7430e287f899507f28f00e5c7be91fddd5fbc0e4
Jordan <Jordan>
2022-07-11 14:29:08 -0400

    fail safes added based on api documentation, coding complete sans tests

--- R/getDataValueSets.R
+++ R/getDataValueSets.R
@@ -7,7 +7,7 @@
 #' @param d2_session R6 datimutils object which handles authentication with DATIM
 #' @param retry number of times to retry
 #' @param timeout number of seconds to wait during call
-#' @return  tibble with the data requested
+#' @return  Data frame with the data requested
 #'
 getDataValueSets <- function(variable_keys = NULL, #keys,
                              variable_values = NULL, #values,
@@ -18,34 +18,47 @@ getDataValueSets <- function(variable_keys = NULL, #keys,
   #Test that the provided variables have their associated values used for munging
   assertthat::assert_that(length(variable_keys) == length(variable_values))
   
+  #Example api call
+  #/api/dataValueSets.json?dataSet=pBOMPrpg1QX&period=201401&orgUnit=DiszpKrYNg8
+  
   #TODO: Consider implementing a check of all paramaters
   #https://docs.dhis2.org/en/develop/using-the-api/dhis-core-version-master/data.html
   
   #Requirements 
   # The following constraints apply to the data value sets resource:
-  #   
-  # At least one data set must be specified.
-  # 
-  # Either at least one period or a start date and end date must be specified.
-  # 
-  # At least one organisation unit must be specified.
-  # 
-  # Organisation units must be within the hierarchy of the organisation units of the authenticated user.
-  # 
-  # Limit cannot be less than zero.
+
+  #1 At least one data set must be specified.
+  if(!(is.element('dataSet', variable_keys))) {
+    stop("At least one data set must be specified.")
+  }
+
+  #2 Either at least one period or a start date and end date must be specified.
+  if((
+    (!(is.element('startDate', variable_keys)) |
+     !(is.element('endDate', variable_keys)))
+    &
+    !(is.element('period', variable_keys))
+    )) {
+    stop("Either at least one period or a start date and end date must be specified.")
+  }
   
-  # /api/dataValueSets.json?dataSet=pBOMPrpg1QX&period=201401&orgUnit=DiszpKrYNg8
+  #3 At least one organisation unit must be specified.
+  if(!(is.element('orgUnit', variable_keys))) {
+    stop("At least one organisation unit must be specified.")
+  }
+
+  #4 Organisation units must be within the hierarchy of the organisation units of the authenticated user.
+    # This should be taken care of by loginToDatim, so wont add at this time.
   
-  if (length(variable_keys) > 0) {
+  #5 Limit cannot be less than zero.
+    # I don't think we need to bother coding this
   
+  # Removed ifelse below, as it should theoretically be impossible to not
+  #    enter variables with the above stop's
   # concatenate and format the keys and values provided for the api call
     variable_k_v_pairs <- mapply(function(x, y) paste0(x, "=", y),
                                  variable_keys, variable_values)
     variable_k_v_pairs <- paste0(variable_k_v_pairs, collapse = "&")
-
-    } else {
-    variable_k_v_pairs <- NULL
-    }
     
     #Create URL Path
     path=paste0("api/dataValueSets.json?",
