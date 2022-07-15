13c4ae386e4c95e5c1bb414755c2249faabda45f
Jordan Bales <89547800+JordanBalesBAO@users.noreply.github.com>
2022-07-15 12:34:20 -0400

    Dp 137 (#91)
    * Bringing function over from Datapackr. Will probably need modification before actual use.
    
    * Refactored function to use apiget. Still working on "checks" and "tests"
    
    * fail safes added based on api documentation, coding complete sans tests
    
    * added test and associated mock api call
    
    * updated namespace file to have the new function
    
    * added .rd to man folder
    
    * adding line to end of file
    
    * fixed linting issues
    
    * updated the function to not rename columns from api response
    
    * added error message
    
    * removed the return from this function, this will save the console output from being cluttered
    
    * updated tests, ready to look at again @Jason
    
    Co-authored-by: Jordan <Jordan>

---
+++ R/getDataValueSets.R
@@ -0,0 +1,86 @@
+#' @export
+#' @title getDataValueSets
+#'
+#' @description Used to read DHIS 2 data using the data value set endpoint
+#' @param keys character vector - data value set parameter keys
+#'  (e.g. "dataSet", "period")
+#' @param values character vector - values marching the key from keys
+#'  (e.g. "Abcde123456", "2019Q1"
+#' @param d2_session R6 datimutils object which handles authentication
+#'  with DATIM
+#' @param retry number of times to retry
+#' @param timeout number of seconds to wait during call
+#' @return  Data frame with the data requested
+#'
+getDataValueSets <- function(variable_keys = NULL, #keys,
+                             variable_values = NULL, #values,
+                             d2_session = dynGet("d2_default_session",
+                                                 inherits = TRUE),
+                             retry=1, timeout = 180) {
+
+  #Test that the provided variables have their associated values used for
+  # munging
+  assertthat::assert_that(length(variable_keys) == length(variable_values),
+    msg = "The number of keys provided is not equal to the number of values provided.
+    Please ensure these match and try again.")
+
+  #Example api call
+  #/api/dataValueSets.json?dataSet=pBOMPrpg1QX&period=201401&orgUnit=DiszpKrYNg8
+
+  #TODO: Consider implementing a check of all paramaters
+  #https://docs.dhis2.org/en/develop/using-the-api/dhis-core-version-master/data.html
+
+  #Requirements
+  # The following constraints apply to the data value sets resource:
+
+  #1 At least one data set must be specified.
+  if (!(is.element("dataSet", variable_keys))) {
+    stop("At least one data set must be specified.")
+  }
+
+  #2 Either at least one period or a start date and end date must be specified.
+  if ((
+    (!(is.element("startDate", variable_keys)) |
+     !(is.element("endDate", variable_keys)))
+    &
+    !(is.element("period", variable_keys))
+    )) {
+    stop("Either at least one period or a start date and end date must be
+         specified.")
+  }
+
+  #3 At least one organisation unit must be specified.
+  if (!(is.element("orgUnit", variable_keys))) {
+    stop("At least one organisation unit must be specified.")
+  }
+
+  #4 Organisation units must be within the hierarchy of the organisation units
+  #    of the authenticated user.
+    # This should be taken care of by loginToDatim, so wont add at this time.
+
+  #5 Limit cannot be less than zero.
+    # I don't think we need to bother coding this
+
+  # Removed ifelse below, as it should theoretically be impossible to not
+  #    enter variables with the above stop's
+  # concatenate and format the keys and values provided for the api call
+    variable_k_v_pairs <- mapply(function(x, y) paste0(x, "=", y),
+                                 variable_keys, variable_values)
+    variable_k_v_pairs <- paste0(variable_k_v_pairs, collapse = "&")
+
+    #Create URL Path
+    path <- paste0("api/dataValueSets.json?",
+         variable_k_v_pairs,
+         "&paging=false")
+
+    resp <- api_get(
+      path = path,
+      d2_session = d2_session,
+      retry = retry,
+      timeout = timeout
+    )
+
+    #Create Dataframe from api response
+    resp <- as.data.frame(resp$dataValues, stringsAsFactors = FALSE)
+
+}
---
+++ tests/testthat/test-getDataValueSets.R
@@ -0,0 +1,37 @@
+context("Test GetDataValueSets")
+
+test_that("GetDataValueSets", {
+  # httptest::start_capturing(simplify = FALSE)
+  # httr::content(httr::GET(paste0(
+  #   "https://play.dhis2.org/2.37.2/api/dataValueSets.json?",
+  #   "dataSet=pBOMPrpg1QX&period=202201&orgUnit=DiszpKrYNg8&paging=false"),
+  #   httr::authenticate("admin", "district")))
+  # httptest::stop_capturing()
+  #Note didn't work due to api_get, so just copied and pasted json to 
+  #correct file
+  
+  with_mock_api({
+    data <- getDataValueSets(c("dataSet", "period", "orgUnit"),
+                             c("pBOMPrpg1QX", "202201", "DiszpKrYNg8"),
+                             d2_session = play2372)
+  
+  
+    testthat::expect_named(data, c("dataElement",
+                                   "period",
+                                   "orgUnit",
+                                   "categoryOptionCombo",
+                                   "attributeOptionCombo",
+                                   "value",
+                                   "storedBy",
+                                   "created",
+                                   "lastUpdated",
+                                   "comment",
+                                   "followup"))
+    testthat::expect_equal(NROW(data), 3)
+    testthat::expect_error(getDataValueSets(c("limit"),
+                                            c("3"),
+                                            play2372),
+                           "At least one data set must be specified.",
+                           fixed=TRUE)
+  })
+})
