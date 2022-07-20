d489588ac1bf3e19c89952cfffdb525410ea850f
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-07-20 15:12:18 -0400

    Linting

--- R/callDATIMapi.R
+++ R/callDATIMapi.R
@@ -83,16 +83,16 @@ api_get <- function(path,
 
   while (i <= retry && (response_code < 400 || response_code >= 500)) {
     resp <- NULL
-    resp <- 
+    resp <-
       try(
       httr::GET(url, httr::timeout(timeout),
                       handle = handle)
       )
-    
-    if(is.null(resp)) {
+
+    if (is.null(resp)) {
       next
     }
-    
+
     response_code <- httr::status_code(resp)
     Sys.sleep(i - 1)
     i <- i + 1
