150a3a9d73c78f3f40e2c2051ea90484e5241744
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-07-22 08:50:52 -0400

    Revert "Update callDATIMapi.R"

--- R/callDATIMapi.R
+++ R/callDATIMapi.R
@@ -82,17 +82,8 @@ api_get <- function(path,
   response_code <- 5
 
   while (i <= retry && (response_code < 400 || response_code >= 500)) {
-    resp <- NULL
-    resp <-
-      try(
-      httr::GET(url, httr::timeout(timeout),
+    resp <- httr::GET(url, httr::timeout(timeout),
                       handle = handle)
-      )
-
-    if (is.null(resp)) {
-      next
-    }
-
     response_code <- httr::status_code(resp)
     Sys.sleep(i - 1)
     i <- i + 1
