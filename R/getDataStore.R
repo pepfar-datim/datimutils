2d003b5ad15f19959453f2eaae956a5b659520e6
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-07-22 09:35:41 -0400

    Linting

--- R/getDataStore.R
+++ R/getDataStore.R
@@ -1,6 +1,6 @@
 
 #' @title getDataStoreKey returns a particular key from the datastore
-#' @description The DHIS2 datastore can be used to store arbitrary JSON objects. 
+#' @description The DHIS2 datastore can be used to store arbitrary JSON objects.
 #' @param namespace Namespace of the datastore
 #' @param key  Key of the datastore
 #' @param simplify Should the JSON response be simplified?
@@ -9,48 +9,47 @@
 #' @param retry Number of times to retry the request
 #' @param timeout Timeout in number of seconds
 #'
-#' @return A list if the request is successful. A data frame may be returned if 
+#' @return A list if the request is successful. A data frame may be returned if
 #' the simiplify paramater is set to TRUE, but it depends on the intrinsic structure
-#' of the key itself. 
+#' of the key itself.
 #' @export
 #'
 
-getDataStoreKey<-function(namespace,key,simplify = FALSE,
-                          d2_session = dynGet("d2_default_session", inherits = TRUE),
-                          retry  = 3,
-                          timeout=60) {
-  
-isSimpleString<-function(x) {
-  !( is.na(x) || x =="" || is.null(x) ) & typeof(x) == "character"
+getDataStoreKey <- function(namespace, key, simplify = FALSE,
+                            d2_session = dynGet("d2_default_session", inherits = TRUE),
+                            retry  = 3,
+                            timeout = 60) {
+
+isSimpleString <- function(x) {
+  !(is.na(x) || x == "" || is.null(x)) & typeof(x) == "character"
 }
-  
-if ( !isSimpleString(namespace) || !isSimpleString(key)) {
+
+if (!isSimpleString(namespace) || !isSimpleString(key)) {
   stop("Please specify the namespace and key!")
 }
-  url<-paste0(d2_session$base_url,"api/dataStore/",namespace,"/",key) 
-  
+  url <- paste0(d2_session$base_url, "api/dataStore/", namespace, "/", key)
+
   # retry api get block, only retries if reponse code not in 400s
   i <- 1
   response_code <- 5
-  
-  while (i <= retry & (response_code < 400 | response_code >= 500)) {
+
+  while (i <= retry && (response_code < 400 || response_code >= 500)) {
     resp <- httr::GET(url, httr::timeout(timeout),
                       handle = d2_session$handle)
     response_code <- httr::status_code(resp)
-    Sys.sleep(i-1)
+    Sys.sleep(i - 1)
     i <- i + 1
-    if(response_code == 200 && 
-       resp$url == url &&
-       httr::http_type(resp) == "application/json")
-    {
+    if (response_code == 200 &&
+        resp$url == url &&
+        httr::http_type(resp) == "application/json") {
       break
     }
   }
-    
+
     if (response_code == 404L) {
       warning("The requested datastore object could not be found")
       return(NULL)
     }
-    
-    jsonlite::parse_json(httr::content(resp,type = "text"),simplifyVector = simplify)
+
+    jsonlite::parse_json(httr::content(resp, type = "text"), simplifyVector = simplify)
 }
--- tests/testthat/test-dataStore.R
+++ tests/testthat/test-dataStore.R
@@ -6,40 +6,39 @@
 #    httr::GET("https://play.dhis2.org/2.36.3/api/dataStore/metabase/repositories",
 #            httr::authenticate("admin", "district"))
 #  )
-# 
+#
 #  httr::content(
 #    httr::GET("https://play.dhis2.org/2.36.3/api/dataStore/metabase/foo",
 #              httr::authenticate("admin", "district"))
 #  )
-#  
+#
 # httptest::stop_capturing()
 
 context("Get a datastore key")
 with_mock_api({
-  
+
   test_that("Get a datstore object ", {
 
-    data <- getDataStoreKey("metabase","repositories",d2_session = play2363)
-    
+    data <- getDataStoreKey("metabase", "repositories", d2_session = play2363)
+
     testthat::expect_type(data, "list")
-    testthat::expect_equal(length(data),2)
+    testthat::expect_equal(length(data), 2)
     rm(data) })
 
-  test_that("Can error with missing namespace",{
-    expect_error(getDataStoreKey(NA,"repositories",d2_session = play2363))
+  test_that("Can error with missing namespace", {
+    expect_error(getDataStoreKey(NA, "repositories", d2_session = play2363))
   })
-  
-  test_that("Can error with missing key",{
-    expect_error(getDataStoreKey(NA,"repositories",d2_session = play2363))
-  })
-  
-  test_that("Can error with a list as namespace",{
 
-    expect_warning(data<-getDataStoreKey("metabase","foo",d2_session = play2363),"The requested datastore object could not be found")
+  test_that("Can error with missing key", {
+    expect_error(getDataStoreKey(NA, "repositories", d2_session = play2363))
+  })
+
+  test_that("Can error with a list as namespace", {
+    expect_warning(data <- getDataStoreKey("metabase", "foo", d2_session = play2363),
+                   "The requested datastore object could not be found")
     expect_null(data)
   })
-  
-  
- 
-})
 
+
+
+})
