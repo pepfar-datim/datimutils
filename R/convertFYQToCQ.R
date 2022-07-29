974c3193ac32056e3937f8e254713bb28f484d98
Maxwell Chandler <61993910+maxwellchandler@users.noreply.github.com>
2022-07-29 06:26:05 -0600

    DP-132: Add `convertFYQuarterCalendarQuarter` (#58)
    * added convertFYQuarterCalendarQuarter
    
    * Shortened function name
    
    * Update file name and namespace
    
    * add .Rd file
    
    * fix merge conflict
    
    * fix merge conflict
    
    * removing
    
    * Revert "removing"
    
    This reverts commit 78ed760754b7065dc1b0a33d143d73dbba859d66.
    
    * Revert "add .Rd file"
    
    This reverts commit 253262e5aef1162bc15681194c467a6a7d4e9363.
    
    * hoping this fixes this old branch
    
    * Fixing merge conflict
    
    * renaming files to conform to standards
    
    * linting
    
    Co-authored-by: Jordan <Jordan>
    Co-authored-by: Jordan Bales <89547800+JordanBalesBAO@users.noreply.github.com>

---
+++ R/convertFYQToCQ.R
@@ -0,0 +1,22 @@
+#' @export
+#' @title convertconvertFYQToCQ
+#' @description converts fiscal year and quarter to calendar year and quarter
+#' @param fiscal_yyyy_int a 4 digit number
+#' @param fiscal_q_int a single number
+#' @return "yyyyQQ"
+#' @example convertFYQToCQ(1995,1)
+convertFYQToCQ <- function(fiscal_yyyy_int, fiscal_q_int) {
+  fiscal_yyyy_int <- as.integer(fiscal_yyyy_int)
+  fiscal_q_int <- as.integer(fiscal_q_int)
+  if (!(fiscal_q_int %in% c(1, 2, 3, 4))) {
+    stop("Invalid fiscal quarter")
+    }
+  if (fiscal_q_int == 1) {
+    calendar_q_int  <-  4
+    calendar_yyyy_int <- fiscal_yyyy_int - 1
+  } else {
+    calendar_q_int <- fiscal_q_int - 1
+    calendar_yyyy_int <- fiscal_yyyy_int
+  }
+  return(paste0(calendar_yyyy_int, "Q", calendar_q_int))
+}
