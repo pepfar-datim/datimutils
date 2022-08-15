5090298a50519316590fcba754fc7277a2f480b8
Fausto Lopez <flopez@baosystems.com>
2022-08-15 13:01:40 -0400

    try bypassing try error
    test fixing try error

--- R/callDATIMapi.R
+++ R/callDATIMapi.R
@@ -87,19 +87,20 @@ api_get <- function(path,
   # retry api get block, only retries if response code not in 400s
   i <- 1
   response_code <- 5
-  resp <- "try-error"
   
-  while (i <= retry && (resp == "try-error"|| response_code < 400 || response_code >= 500)) {
-    #resp <- NULL
+  while (i <= retry && (response_code < 400 || response_code >= 500)) {
+    resp <- NULL
     resp <-
       try(
       httr::GET(url, httr::timeout(timeout),
                       handle = handle)
       )
 
-    response_code <- httr::status_code(resp)
+    # try is added in order to handle if resp comes back as a "try-error" class
+    response_code <- try(httr::status_code(resp))
     Sys.sleep(i - 1)
     i <- i + 1
+    
     if (response_code == 200L &&
         stringi::stri_replace(resp$url, regex = ".*/api/", replacement = "") ==
         stringi::stri_replace(url, regex = ".*/api/", replacement = "") &&
@@ -107,6 +108,17 @@ api_get <- function(path,
       break
     }
 
+  }
+  
+  # let user know that by the last attempt the api continues to return an error, this should break before status code
+  # as a status code cannot be pulled from a failed api grab
+  if(class(resp) == "try-error") {
+    stop(
+      paste0(
+        "response error, server returned no response even on the last retry, this could a malformed link or a server issue, otherwise try again ",
+        url
+      )
+    )
   }
 
   # unknown error catching which returns message and response code
