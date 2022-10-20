5c47f5e381c78600f61123c7153cf44241745099
Jordan <Jordan>
2022-10-20 11:56:21 -0400

    app gutted for demo purposes

--- inst/shiny-examples/OAuth/server.R
+++ inst/shiny-examples/OAuth/server.R
@@ -1,11 +1,18 @@
-pacman::p_load(shiny, shinyjs, shinyWidgets, magrittr, dplyr,
-               datimvalidation, ggplot2, datimutils,
-               futile.logger, paws, datapackr, scales,
-               DT, purrr, rpivotTable, waiter,
-               flextable, officer, gdtools, digest, fansi)
+### Libraries
+library(shiny)
+library(httr)
+library(shinydashboard)
+library(magrittr)
+library(tidyverse)
+library(xml2)
+library(datimutils)
+library(waiter)
+library(futile.logger)
+library(shinyWidgets)
 
-# js ----
-# allows for using the enter button
+
+### js ----
+# allows for using the enter button for the log in
 jscode_login <- '$(document).keyup(function(e) {
     var focusedElement = document.activeElement.id;
     console.log(focusedElement);
@@ -16,20 +23,11 @@ jscode_login <- '$(document).keyup(function(e) {
     }
 });'
 
-#Set the maximum file size for the upload file
-options(shiny.maxRequestSize = 150 * 1024 ^ 2)
-#Allow unsanitized error messages
-options(shiny.sanitize.errors = FALSE)
-#Initiate logging
+### Initiate logging
 logger <- flog.logger()
+flog.appender(appender.console(), name = "datimutils")
 
-if (!file.exists(Sys.getenv("LOG_PATH"))) {
-    file.create(Sys.getenv("LOG_PATH"))
-}
-
-flog.appender(appender.console(), name = "datapack")
-
-################ OAuth Client information #####################################
+### OAuth Client information
 if (interactive()) {
     # testing url
     options(shiny.port = 3123)
@@ -42,13 +40,13 @@ if (interactive()) {
 {
     
     oauth_app <- httr::oauth_app(Sys.getenv("OAUTH_APPNAME"),
-                                 key = Sys.getenv("OAUTH_KEYNAME"),        # dhis2 = Client ID
+                                 key = Sys.getenv("OAUTH_KEYNAME"), # dhis2 = Client ID
                                  secret = Sys.getenv("OAUTH_SECRET"), #dhis2 = Client Secret
                                  redirect_uri = APP_URL
     )
     
     oauth_api <- httr::oauth_endpoint(base_url = paste0(Sys.getenv("BASE_URL"),"uaa/oauth"),
-                                      request=NULL,# Documentation says to leave this NULL for OAuth2
+                                      request=NULL,
                                       authorize = "authorize",
                                       access="token"
     )
@@ -61,54 +59,23 @@ has_auth_code <- function(params) {
     return(!is.null(params$code))
 }
 
-
-
+###  Begin Shiny Web app items
 shinyServer(function(input, output, session) {
     
     validation_results <- reactive({ validate() }) # nolint
     
     ready <- reactiveValues(ok = FALSE)
     
-    
     user_input <- reactiveValues(authenticated = FALSE,
                                  status = "",
                                  d2_session = NULL,
                                  memo_authorized = FALSE,
                                  uuid = NULL)
-    
-    
-    observeEvent(input$file1, {
-        shinyjs::show("validate")
-        shinyjs::enable("validate")
-        ready$ok <- FALSE
-    })
-    
-    observeEvent(input$validate, {
-        shinyjs::disable("file1")
-        shinyjs::disable("validate")
-        ready$ok <- TRUE
-    })
-    
-    observeEvent(input$reset_input, {
-        shinyjs::reset("side-panel")
-        shinyjs::enable("file1")
-        shinyjs::disable("validate")
-        shinyjs::disable("downloadDataPack")
-        shinyjs::disable("download_messages")
-        shinyjs::disable("send_paw")
-        shinyjs::disable("downloadValidationResults")
-        shinyjs::disable("compare")
-        ready$ok <- FALSE
-    })
-    
 
-    
-    
-
-    
+###  Logout
     observeEvent(input$logout, {
         req(input$logout)
-        # Gets you back to the login without the authorization code at top
+        # Returns to the log in screen without the authorization code at top
         updateQueryString("?",mode="replace",session=session)
         flog.info(paste0("User ", user_input$d2_session$me$userCredentials$username, " logged out."))
         ready$ok <- FALSE
@@ -119,13 +86,13 @@ shinyServer(function(input, output, session) {
         d2_default_session <- NULL
         gc()
         session$reload()
-        
     })
-    
+ 
+###  Logic for which UI to display       
     output$ui <- renderUI({
         
         if (user_input$authenticated == FALSE) {
-            ##### UI code for login page
+            # References the UI code for log in page, could be combined
             fluidPage(
                 fluidRow(
                     column(width = 2, offset = 5,
@@ -135,138 +102,42 @@ shinyServer(function(input, output, session) {
                 )
             )
         } else {
+            #References the UI code for after log in, could be combined
             uiOutput("authenticated")
         }
     })
-    
-    
-    # Username and password text fields, login button
+
+### UI code for log in screen
+    # Username and password text fields, log in button
     output$uiLogin <- renderUI({
         
         wellPanel(fluidRow(
-            #img(src = "pepfar.png", align = "center"),
             tags$head(tags$script(HTML(jscode_login))), # enter button functionality for login button
-            tags$div(HTML('<center><img src="pepfar.png"></center>')),
-            h4("Welcome to the DataPack Validation App. Please login with your DATIM credentials:")
+            #tags$div(HTML('<center><img src="pepfar.png"></center>')), #Can add logo
+            h4("Welcome to the Datimutils OAuth Example App. Please login with your DATIM credentials:")
         ),
         fluidRow(
             actionButton("login_button_oauth","Log in with DATIM"),
             uiOutput("ui_hasauth"),
             uiOutput("ui_redirect")
         ),
-        fluidRow(
-            tags$hr(),
-            tags$div(HTML("<ul><li><h4>Please be sure you fully populate the PSNUxIM",
-                          " tab when receiving a new DataPack. Consult <a href = ",
-                          "\"https://apps.datim.org/datapack-userguide/\" target = ",
-                          "\"blank\" > the user guide</a> for further information!",
-                          "</h4></li><li><h4>See the latest updates to the app <a href =",
-                          "\"https://github.com/pepfar-datim/datapackr-app/blob/master/CHANGELOG.md\"",
-                          "target = \"blank\">here.</h4></a></li></ul>"))
-        ),
-        tags$hr(),
-        fluidRow(HTML(getVersionInfo())))
+
+        )
     })
-    
+
+### UI code for after log in      
     output$authenticated <- renderUI({
-        wiki_url <- a("Datapack User Guide",
-                      href = "https://apps.datim.org/datapack-userguide/",
-                      target = "_blank")
         
         fluidPage(
-            tags$head(tags$style(".shiny-notification {
-                             position: fixed;
-                             top: 10%;
-                             left: 33%;
-                             right: 33%;}")),
-            use_waiter(),
-            sidebarLayout(
-                sidebarPanel(
-                    shinyjs::useShinyjs(),
-                    id = "side-panel",
-                    tagList(wiki_url),
-                    tags$hr(),
-                    fileInput(
-                        "file1",
-                        "Choose DataPack (Must be XLSX!):",
-                        accept = c("application/xlsx",
-                                   ".xlsx"),
-                        width = "240px"
-                    ),
-                    actionButton("validate", "Validate"),
-                    tags$hr(),
-                    selectInput("downloadType", "Download type", NULL),
-                    downloadButton("downloadOutputs", "Download"),
-                    tags$hr(),
-                    actionButton("send_paw", "Send to PAW"),
-                    tags$hr(),
-                    div(style = "display: inline-block; vertical-align:top; width: 80 px;",
-                        actionButton("reset_input", "Reset inputs")),
-                    div(style = "display: inline-block; vertical-align:top; width: 80 px;",
-                        actionButton("logout", "Logout"))
-                ),
-                
-                mainPanel(tabsetPanel(
-                    id = "main-panel",
-                    type = "tabs",
-                    tabPanel("Messages", tags$ul(uiOutput("messages"))),
-                    tabPanel("Analytics checks", tags$div(uiOutput("analytics_checks"))),
-                    tabPanel("Indicator summary", dataTableOutput("indicator_summary"),
-                             tags$h4("Data source: Main DataPack tabs")),
-                    tabPanel("SNU-level summary",
-                             dataTableOutput("snu_summary"),
-                             tags$h4("Data source: Main DataPack tabs")),
-                    tabPanel("Validation rules",
-                             dataTableOutput("vr_rules"),
-                             tags$h4("Data source: PSNUxIM tab")),
-                    tabPanel("HTS Summary Chart",
-                             fluidRow(column(width = 12, div(style = "height:700px", plotOutput("modality_summary")))),
-                             fluidRow(column(width = 12, tags$h4("Data source: PSNUxIM tab")))),
-                    tabPanel("HTS Summary Table",
-                             dataTableOutput("modality_table"),
-                             tags$h4("Data source: PSNUxIM tab")),
-                    tabPanel("HTS Yield",
-                             fluidRow(column(width = 12, div(style = "height:700px", plotOutput("modality_yield")))),
-                             fluidRow(tags$h4("Data source: PSNUxIM tab"))),
-                    tabPanel("HTS Recency",
-                             dataTableOutput("hts_recency"),
-                             tags$h4("Data source: PSNUxIM tab")),
-                    tabPanel("VLS Testing",
-                             fluidRow(column(width = 12, div(style = "height:700px", plotOutput("vls_summary")))),
-                             fluidRow(column(width = 12, tags$h4("Data source: PSNUxIM tab")))),
-                    tabPanel("Epi Cascade Pyramid",
-                             pickerInput("epiCascadeInput", "SNU1",
-                                         choices = "",
-                                         options = list(`actions-box` = TRUE), multiple = T),
-                             fluidRow(column(width = 12, div(style = "height:700px", plotOutput("epi_cascade")))),
-                             fluidRow(tags$h4("Data source: SUBNATT/IMPATT data & PSNUxIM tab"))),
-                    tabPanel("KP Cascade Pyramid",
-                             pickerInput("kpCascadeInput", "SNU1",
-                                         choices = "",
-                                         options = list(`actions-box` = TRUE), multiple = T),
-                             fluidRow(column(width = 12, div(style = "height:700px", plotOutput("kp_cascade")))),
-                             fluidRow(tags$h4("Data source: Data source: SUBNATT/IMPATT data & PSNUxIM tab"))),
-                    tabPanel("PSNUxIM Pivot",
-                             fluidRow(column(width = 12, div(rpivotTable::rpivotTableOutput({"pivot"})))), # nolint
-                             fluidRow(tags$h4("Data source: PSNUxIM tab"))),
-                    tabPanel(
-                        "Memo Tables",
-                        fluidRow(
-                            pickerInput(
-                                "memo_pivot_style",
-                                label = "Table Style",
-                                choices = c("Prioritization", "By Agency", "By Partner", "Comparison"),
-                                selected = "Prioritization"
-                            )
-                        ),
-                        fluidRow(column(width = 12,
-                                        div(dataTableOutput({"memo_compare"})))), # nolint
-                        fluidRow(tags$h4("Data source: PSNUxIM tab & DATIM")))
-                    
-                ))
-            ))
+            titlePanel("OAuth or PW Login Template"),
+            DT::dataTableOutput("mytable"),
+            actionButton("logout",
+                         "Return to Login Page",
+                         icon=icon("sign-out"))
+        )
     })
-    
+
+ #UI that will display when redirected to OAuth login agent
     output$ui_redirect = renderUI({
         #print(input$login_button_oauth) useful for debugging
         if(!is.null(input$login_button_oauth)){
@@ -277,8 +148,8 @@ shinyServer(function(input, output, session) {
             } else NULL
         } else NULL
     })
-    
-    ### Login Button oauth Checks
+
+### Login Button oauth Checks
     observeEvent(input$login_button_oauth > 0,{
         
         #Grabs the code from the url
@@ -312,7 +183,7 @@ shinyServer(function(input, output, session) {
             ) },
             # This function throws an error if the login is not successful
             error = function(e) {
-                flog.info(paste0("User ", input$user_name, " login failed. ", e$message), name = "datapack")
+                flog.info(paste0("User ", input$user_name, " login failed. ", e$message), name = "datimutils")
             }
         )
         
@@ -335,9 +206,9 @@ shinyServer(function(input, output, session) {
                     user_input$d2_session$me$userCredentials$username,
                     " logged in."
                 ),
-                name = "datapack"
+                name = "datimutils"
             )
-            sendEventToS3(NULL, "LOGIN", user_input = user_input)
+            
             
             flog.info(
                 paste0(
@@ -345,10 +216,20 @@ shinyServer(function(input, output, session) {
                     user_input$d2_session$me$userCredentials$username,
                     " logged in."
                 ),
-                name = "datapack"
+                name = "datimutils"
             )
         }
         
     })
-    
+
+########## Below this line is your standard server code ##########
+### Controls Data Table   
+    output$mytable = DT::renderDataTable({
+        
+        df=getMetadata(
+            end_point = "organisationUnitGroups",
+            fields = "id,name",
+            d2_session = user_input$d2_session
+        )
+    })
 })
