77bcc77b8d1c8e12831cb53be6746b188bac5d35
sam-bao <sgarman@baosystems.com>
2023-09-05 18:03:13 +0200

    More mock updates

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,13 +1,9 @@
 library(httptest)
 play40.0.1 <- list(base_url = "https://play.dhis2.org/40.0.1/",
                   handle = httr::handle("https://play.dhis2.org/40.0.1/"))
-play2335 <- list(base_url = "https://play.dhis2.org/2.33.5/",
-                handle = httr::handle("https://play.dhis2.org/2.33.5/"))
+play2.37.10 <- list(base_url = "https://play.dhis2.org/2.37.10/",
+                   handle = httr::handle("https://play.dhis2.org/2.37.10/"))
 play2363 <- list(base_url = "https://play.dhis2.org/2.36.3/",
                  handle = httr::handle("https://play.dhis2.org/2.36.3/"))
-play2372 <- list(base_url = "https://play.dhis2.org/2.37.2/",
-                 handle = httr::handle("https://play.dhis2.org/2.37.2/"))
-play23513 <- list(base_url = "https://play.dhis2.org/2.35.13/",
-                 handle = httr::handle("https://play.dhis2.org/2.35.13/"))
 test <- list(base_url = "https://test.datim.org/",
                  handle = httr::handle("https://test.datim.org/"))
