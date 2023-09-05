5e4a07f985adddc2166d140a42583e1b1f828f9c
sam-bao <sgarman@baosystems.com>
2023-09-05 19:59:00 +0200

    Update helper.R

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,7 +3,5 @@ play40.0.1 <- list(base_url = "https://play.dhis2.org/40.0.1/",
                   handle = httr::handle("https://play.dhis2.org/40.0.1/"))
 play2.37.10 <- list(base_url = "https://play.dhis2.org/2.37.10/",
                    handle = httr::handle("https://play.dhis2.org/2.37.10/"))
-play2363 <- list(base_url = "https://play.dhis2.org/2.36.3/",
-                 handle = httr::handle("https://play.dhis2.org/2.36.3/"))
 test <- list(base_url = "https://test.datim.org/",
                  handle = httr::handle("https://test.datim.org/"))
